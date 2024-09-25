0c628a90b95cded1a69c54ea6975af88810d401a
GitHub Action <michael+cicd@offisito.com>
2024-09-25 03:30:15 +0300

    nav and structure

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,39 +1,7 @@
+import { Stack, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
-import InputLabel from "@mui/material/InputLabel";
-import Link from "@mui/material/Link";
-import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
-
-import { visuallyHidden } from "@mui/utils";
-import { styled } from "@mui/material/styles";
-
-const StyledBox = styled("div")(({ theme }) => ({
-  alignSelf: "center",
-  width: "100%",
-  height: 400,
-  marginTop: theme.spacing(8),
-  borderRadius: theme.shape.borderRadius,
-  outline: "6px solid",
-  outlineColor: "hsla(220, 25%, 80%, 0.2)",
-  border: "1px solid",
-  borderColor: theme.palette.grey[200],
-  boxShadow: "0 0 12px 8px hsla(220, 25%, 80%, 0.2)",
-  backgroundImage: `url(${"/static/screenshots/material-ui/getting-started/templates/dashboard.jpg"})`,
-  backgroundSize: "cover",
-  [theme.breakpoints.up("sm")]: {
-    marginTop: theme.spacing(10),
-    height: 700,
-  },
-  ...theme.applyStyles("dark", {
-    boxShadow: "0 0 24px 12px hsla(210, 100%, 25%, 0.2)",
-    backgroundImage: `url(${"/static/screenshots/material-ui/getting-started/templates/dashboard-dark.jpg"})`,
-    outlineColor: "hsla(220, 20%, 42%, 0.1)",
-    borderColor: theme.palette.grey[700],
-  }),
-}));
 
 export default function Hero() {
   return (
@@ -144,21 +112,7 @@ export default function Hero() {
               Get MikNTech
             </Button>
           </Stack>
-          {/*<Typography
-            variant="caption"
-            color="text.secondary"
-            sx={{ textAlign: "center" }}
-          >
-            By clicking &quot;Start now&quot; you agree to our&nbsp;
-            <Link href="#" color="primary">
-              Terms & Conditions
-            </Link>
-            .
-          </Typography>*/}
         </Stack>
-        {/*
-        <StyledBox id="image" />
-*/}
       </Container>
     </Box>
   );
